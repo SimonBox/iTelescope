1669a4d16fef40de788fe9f0f7af3649f2db9314
Simon <simon@StudioLaptop.(none)>
2011-11-13 13:50:59 +0000

    First upload of code

---
+++ TelescopeServerCode/R2Duino.hpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   R2Duino.hpp
+ * Author: simon
+ *
+ * Created on 20 January 2011, 17:20
+ */
+
+#ifndef R2DUINO_HPP
+#define	R2DUINO_HPP
+
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class R2Duino {
+public:
+
+    //class members
+    FILE *infile;
+    double RA,DE;
+
+    //class constructor
+    R2Duino();
+
+    //class Functions
+    void RunLoop();
+
+
+
+};
+
+#endif	/* R2DUINO_HPP */
+
